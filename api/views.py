715a4d1ca8361e4dbea411199b41f10d3e6e7aef
Luis Herrera <pipeh.ing@gmail.com>
2022-06-30 22:50:19 -0500

    feat: crud equipos

--- api/views.py
+++ api/views.py
@@ -1,21 +1,56 @@
-from api.models import Equipo
-from django.shortcuts import get_object_or_404
-from api.serializers import EquipoSerializer
-from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from api.models import Equipo
+from api.serializers import EquipoSerializer
 
 # Create your views here.
-class EquipoViewSet(viewsets.ViewSet):
+class EquipoViewSet(ViewSet):
     """
     Endpoint para el modelo equipo.
     """
+    
+    def create(self, request):
+        serializer = EquipoSerializer(data=request.data)
+        
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED, data=serializer.data)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
     def list(self, request):
-        queryset = Equipo.objects.all()
-        serializer = EquipoSerializer(queryset, many=True)
+        equipo = Equipo.objects.all()
+        serializer = EquipoSerializer(equipo, many=True)
         return Response(serializer.data)
 
     def retrieve(self, request, pk=None):
-        queryset = Equipo.objects.all()
-        user = get_object_or_404(queryset, pk=pk)
-        serializer = EquipoSerializer(user)
+        equipo = get_object_or_404(Equipo, pk=pk)
+        serializer = EquipoSerializer(equipo)
         return Response(serializer.data)
+    
+    def update(self, request, pk=None):
+        equipo = get_object_or_404(Equipo, id=pk)
+        serializer = EquipoSerializer(instance=equipo, data=request.data)
+        
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(status=status.HTTP_200_OK, data=serializer.data)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def partial_update(self, request, pk=None):
+        equipo = get_object_or_404(Equipo.objects.all(), id=pk)
+        serializer = EquipoSerializer(instance=equipo, data=request.data, partial= True)
+        
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(status=status.HTTP_200_OK, data=serializer.data)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def destroy(self, request, pk=None):
+        equipo = get_object_or_404(Equipo, id=pk)
+        equipo.delete()
+        return Response(status=status.HTTP_202_ACCEPTED,data='borrado con exito')
