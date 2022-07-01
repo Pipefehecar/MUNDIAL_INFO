07c308ae40a770487f60ace5a0b9ce874044f712
Luis Herrera <pipeh.ing@gmail.com>
2022-06-30 20:47:21 -0500

    feat: Models configuration and choices file

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+@admin.register(Equipo)
+class EquipoAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'created_at']
+    
+@admin.register(Jugador)
+class JugadorAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'apellido', 'posicion', 'es_titular', 'equipo_id']
+    
+@admin.register(Directivo)
+class DirectivoAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'apellido','rol', 'equipo_id']
--- api/models.py
+++ api/models.py
@@ -1,3 +1,58 @@
 from django.db import models
-
+from .model_choices import EQUIPOS_ADMITIDOS, POSICIONES_DE_JUEGO, ROLES_ADMINISTRATIVOS
 # Create your models here.
+class Equipo(models.Model):
+    """
+    Modelo para equipos.
+    """
+    nombre = models.CharField(max_length=14, choices=EQUIPOS_ADMITIDOS, unique=True)#los equipos clasificados al mundial son un listado selecto, usamos choices
+    bandera_img = models.CharField(max_length=350, blank=True, null=True)
+    escudo_img = models.CharField(max_length=350,  blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        db_table = 'equipo'
+        
+    def __str__(self):
+        return self.nombre.capitalize() 
+    
+    
+class Jugador(models.Model):
+    """
+    Modelo para jugadores.
+    """
+    foto = models.CharField(max_length=350, blank=True, null=True)
+    nombre = models.CharField(max_length=80)
+    apellido = models.CharField(max_length=80)
+    fecha_nacimiento = models.DateField()
+    numero_camiseta = models.PositiveIntegerField()
+    es_titular = models.BooleanField(default=True)
+    posicion = models.CharField(max_length=3, choices=POSICIONES_DE_JUEGO)
+    equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE, related_name='jugadores')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ['nombre', 'apellido','equipo_id']
+        db_table = 'jugador'
+        verbose_name_plural = 'jugadores'
+    def __str__(self):
+        return self.nombre.capitalize()   
+    
+
+class Directivo(models.Model):
+    """
+    Modelo para cuerpo tecnico.
+    """
+    nombre = models.CharField(max_length=80)
+    apellido = models.CharField(max_length=80)
+    fecha_nacimiento = models.DateField()
+    nacionalidad = models.CharField(max_length=30)
+    rol = models.CharField(max_length=3, choices=ROLES_ADMINISTRATIVOS)
+    equipo_id = models.ForeignKey(Equipo, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        db_table = 'directivo'
+        
+    def __str__(self):
+        return self.nombre.capitalize() 
